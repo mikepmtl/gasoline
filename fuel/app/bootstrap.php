dceff6b5b5d89a7e0666a53b95bf8f3f566bfae1
Philipp Tempel <phtempel@gmail.com>
2013-10-19 15:50:39 +0200

    Add config for pkg auth, also load pkg auth by default

--- fuel/app/bootstrap.php
+++ fuel/app/bootstrap.php
@@ -10,9 +10,6 @@ require COREPATH.'bootstrap.php';
 // Define the constant GASPATH pointing to our gasoline-package
 define('GASPATH', realpath(__DIR__ . '/../gasoline/') . DIRECTORY_SEPARATOR);
 
-// And require the bootstrap (which does some namespace-adding, etc.)
-require(GASPATH . 'bootstrap.php');
-
 Autoloader::add_classes(array(
 	// Add classes you want to override here
 	// Example: 'View' => APPPATH.'classes/view.php',
@@ -20,6 +17,9 @@ Autoloader::add_classes(array(
 
 // Register the autoloader
 Autoloader::register();
+
+// And require the bootstrap (which does some namespace-adding, etc.)
+require(GASPATH . 'bootstrap.php');
 
 /**
  * Your environment.  Can be set to any of the following:

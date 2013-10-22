3cc1b67bd7c5a5a55dd87279f963114c2e1e5043
Philipp Tempel <phtempel@gmail.com>
2013-10-22 12:42:28 +0200

    Update Message\Item with docblocks

--- fuel/packages/message/classes/item.php
+++ fuel/packages/message/classes/item.php
@@ -4,7 +4,7 @@
  * Part of the Gasoline framework
  *
  * @package     Gasoline\Message
- * @version     0.1-dev
+ * @version     1.0-dev
  * @author      Gasoline Development Teams
  * @license     MIT License
  * @copyright   2013 Gasoline Development Team
@@ -13,26 +13,104 @@
 
 class Item {
     
+    /**
+     * Class initialization
+     * 
+     * Just to make sure the message config is set we will load it
+     * 
+     * @static
+     * @access  public
+     * 
+     * @return  void
+     */
     public static function _init()
     {
         \Config::load('message', true);
     }
     
     
-    protected $flash = false;
+    /**
+     * Forge a new message item object
+     * 
+     * @static
+     * @access  public
+     * @param   string  $type       Message type. Mainly used on rendering
+     * @param   string  $message    Actual message to display
+     * @param   string  $heading    Optionally a heading (if supported by the
+     *                              rendering engine [theme or plain html])
+     * @param   array  $attributes  Array of attributes to set on the rendered message
+     *                              item like class, id, ...
+     * 
+     * @return  \Message\Item       Returns message item
+     */
+    public static function forge($type, $message, $heading = null, $attributes = array())
+    {
+        return new static($type, $message, $heading, $attributes);
+    }
     
-    protected $type;
     
-    protected $heading;
     
-    protected $message;
     
+    
+    /**
+     * Array of attributes to set on rendering
+     * 
+     * @access  protected
+     * @var     array
+     */
     protected $attributes = array();
     
-    protected $id = null;
+    /**
+     * Whether the message is a flash message
+     * 
+     * @access  protected
+     * @var     boolean
+     */
+    protected $flash = false;
+    
+    /**
+     * Message heading
+     * 
+     * @access  protected
+     * @var     string
+     */
+    protected $heading;
+    
+    /**
+     * Message's message
+     * 
+     * @access  protected
+     * @var     string
+     */
+    protected $message;
+    
+    /**
+     * Type of the message like info, warning, error, success
+     * 
+     * @access  protected
+     * @var     string
+     */
+    protected $type;
     
     
     
+    
+    
+    /**
+     * Create a new message item
+     * 
+     * @static
+     * @access  public
+     * @param   string  $type       Message type. Mainly used on rendering. If preceeded
+     *                              with "flash:" the message will be stoed
+     * @param   string  $message    Actual message to display
+     * @param   string  $heading    Optionally a heading (if supported by the
+     *                              rendering engine [theme or plain html])
+     * @param   array  $attributes  Array of attributes to set on the rendered message
+     *                              item like class, id, ...
+     * 
+     * @return  \Message\Item       Returns message item
+     */
     public function __construct($type, $message, $heading = null, $attributes = array())
     {
         // That's how we define flash items: Prepend "flash:" before the type and
@@ -62,7 +140,15 @@ class Item {
     }
     
     
-    
+    /**
+     * Get any property of the object
+     * 
+     * @access  public
+     * @param   string  $property   Property name to grade
+     * @throws  InvalidArgumentException    If the property does not exist
+     * 
+     * @return  mixed               Value of the property is returned
+     */
     public function get($property)
     {
         if ( ! property_exists($this, $property) )
@@ -74,12 +160,29 @@ class Item {
     }
     
     
+    /**
+     * Check to see if the message is flash
+     * 
+     * @access  public
+     * 
+     * @return  boolean     True if it's a flash message, otherwise false
+     */
     public function is_flash()
     {
         return $this->flash == true;
     }
     
     
+    /**
+     * Set any of the items property
+     * 
+     * @access  public
+     * @param   string  $property   Name of the property to set
+     * @param   mixed   $value      Value to set
+     * @throws  InvalidArgumentException    If the property does not exist
+     * 
+     * @return  \Message\Item
+     */
     public function set($property, $value = null)
     {
         if ( ! property_exists($this, $property) )
@@ -92,7 +195,13 @@ class Item {
         return $this;
     }
     
-    
+    /**
+     * Converts the object to an array
+     * 
+     * @access  public
+     * @return  array       Associative array of item's properties and their
+     *                      respective values
+     */
     public function to_array()
     {
         return array(
@@ -104,13 +213,16 @@ class Item {
     }
     
     
-    public function render()
-    {
-        // Find the view of this
-    }
-    
-    
-    
+    /**
+     * Magic call to magically get or set a property
+     * 
+     * @access  public
+     * @param   string  $method     Name of method to be called
+     * @param   array   $arguments  Array of arguments passed to method call
+     * @throws  \BadMethodCallException     If the method does not exist
+     * 
+     * @return  mixed               Returns return value of the get or set method
+     */
     public function __call($method, $arguments)
     {
         if ( preg_match('/(?<method>(s|g)et)\_(?<property>[a-zA-Z0-9]+)/', $method, $matches) )
@@ -122,21 +234,30 @@ class Item {
     }
     
     
+    /**
+     * Magic get
+     * 
+     * @access  public
+     * @param   string  $property   Name of property to get
+     * 
+     * @return  mixed               Value of $property is returned
+     */
     public function __get($property)
     {
         return $this->get($property);
     }
     
     
+    /**
+     * Magic set
+     * 
+     * @access  public
+     * @param   string  $property   Name of property to set
+     * @param   mixed   $value      Value to assign to property
+     */
     public function __set($property, $value)
     {
         return $this->set($property, $value);
-    }
-    
-    
-    public function __toString()
-    {
-        return $this->render();
     }
     
 }

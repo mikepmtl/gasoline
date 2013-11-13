1fcd4472af4c077cb3c23573a2707a6f8f7340f8
Philipp Tempel <phtempel@gmail.com>
2013-11-13 15:29:37 +0100

    Update gasoline/bootstrap.php to use __DIR__ rather than GASPATH

--- fuel/gasoline/bootstrap.php
+++ fuel/gasoline/bootstrap.php
@@ -21,43 +21,46 @@
 Autoloader::add_core_namespace('Gasoline', true);
 
 // Also add the whole namespace
-Autoloader::add_namespace('Gasoline', GASPATH . 'classes/');
+Autoloader::add_namespace('Gasoline', __DIR__ . 'classes/');
 
 // This is needed because these classes would actually reside under namespace Gasoline
 //  but since they need to be globally accessible, we add them here as classes
 Autoloader::add_classes(array(
     // Classes for the auth package cannot be added with the gasoline namespace
     // as they must be within the global namespace to be accessible
-    'Auth_Acl_Gasacl'       => GASPATH . '/classes/auth/acl/gasacl.php',
-    'Auth_Group_Gasgroup'   => GASPATH . '/classes/auth/group/gasgroup.php',
-    'Auth_Login_Gasauth'    => GASPATH . '/classes/auth/login/gasauth.php',
+    'Auth_Acl_Gasacl'       => __DIR__ . '/classes/auth/acl/gasacl.php',
+    'Auth_Group_Gasgroup'   => __DIR__ . '/classes/auth/group/gasgroup.php',
+    'Auth_Login_Gasauth'    => __DIR__ . '/classes/auth/login/gasauth.php',
     
-    'Gasoline\\DataContainer'   => GASPATH . '/classes/datacontainer.php',
-    'Gasoline\\Helpers'         => GASPATH . '/classes/helpers.php',
-    'Gasoline\\Html'            => GASPATH . '/classes/html.php',
-    'Gasoline\\Lang'            => GASPATH . '/classes/lang.php',
-    'Gasoline\\Str'             => GASPATH . '/classes/str.php',
-    'Gasoline\\Validation'      => GASPATH . '/classes/validation.php',
-    'Gasoline\\ViewModel'       => GASPATH . '/classes/viewmodel.php',
+    'Gasoline\\Asset_Instance'  => __DIR__ . '/classes/asset/instance.php',
+    'Gasoline\\DataContainer'   => __DIR__ . '/classes/datacontainer.php',
+    'Gasoline\\Helpers'         => __DIR__ . '/classes/helpers.php',
+    'Gasoline\\Html'            => __DIR__ . '/classes/html.php',
+    'Gasoline\\Lang'            => __DIR__ . '/classes/lang.php',
+    'Gasoline\\Str'             => __DIR__ . '/classes/str.php',
+    'Gasoline\\Validation'      => __DIR__ . '/classes/validation.php',
+    'Gasoline\\ViewModel'       => __DIR__ . '/classes/viewmodel.php',
     
-    'Gasoline\\Model\\Base'     => GASPATH . '/classes/model/base.php',
+    'Gasoline\\Controller\\Base'    => __DIR__ . '/classes/controller/base.php',
     
-    'Gasoline\\Model\\Auth_Group'               => GASPATH . '/classes/model/auth/group.php',
-    'Gasoline\\Model\\Auth_Group_Permission'    => GASPATH . '/classes/model/auth/group/permission.php',
-    'Gasoline\\Model\\Auth_Metadata'            => GASPATH . '/classes/model/auth/metadata.php',
-    'Gasoline\\Model\\Auth_Permission'          => GASPATH . '/classes/model/auth/permission.php',
-    'Gasoline\\Model\\Auth_Role'                => GASPATH . '/classes/model/auth/role.php',
-    'Gasoline\\Model\\Auth_Role_Permission'     => GASPATH . '/classes/model/auth/role/permission.php',
-    'Gasoline\\Model\\Auth_User'                => GASPATH . '/classes/model/auth/user.php',
-    'Gasoline\\Model\\Auth_User_Permission'     => GASPATH . '/classes/model/auth/user/permission.php',
+    'Gasoline\\Model\\Base'     => __DIR__ . '/classes/model/base.php',
     
-    'Gasoline\\Orm\\Observer_Id'    => GASPATH . '/classes/orm/observer/id.php',
-    'Gasoline\\Orm\\Observer_Slug'  => GASPATH . '/classes/orm/observer/slug.php',
+    'Gasoline\\Model\\Auth_Group'               => __DIR__ . '/classes/model/auth/group.php',
+    'Gasoline\\Model\\Auth_Group_Permission'    => __DIR__ . '/classes/model/auth/group/permission.php',
+    'Gasoline\\Model\\Auth_Metadata'            => __DIR__ . '/classes/model/auth/metadata.php',
+    'Gasoline\\Model\\Auth_Permission'          => __DIR__ . '/classes/model/auth/permission.php',
+    'Gasoline\\Model\\Auth_Role'                => __DIR__ . '/classes/model/auth/role.php',
+    'Gasoline\\Model\\Auth_Role_Permission'     => __DIR__ . '/classes/model/auth/role/permission.php',
+    'Gasoline\\Model\\Auth_User'                => __DIR__ . '/classes/model/auth/user.php',
+    'Gasoline\\Model\\Auth_User_Permission'     => __DIR__ . '/classes/model/auth/user/permission.php',
+    
+    'Gasoline\\Orm\\Observer_Id'    => __DIR__ . '/classes/orm/observer/id.php',
+    'Gasoline\\Orm\\Observer_Slug'  => __DIR__ . '/classes/orm/observer/slug.php',
 ));
 
-// We also need to add the GASPATH to the finder-instance so we can load config
+// We also need to add the __DIR__ to the finder-instance so we can load config
 //  and other stuff from there
-Finder::instance()->add_path(GASPATH, 1);
+Finder::instance()->add_path(__DIR__, 1);
 
 // Register a function to the 'shutdown'-event to store the currently accessed page
 //  to a session-variable (for redirecting back and forth). This will only happen,

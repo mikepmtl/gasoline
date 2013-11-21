63f59bac61b4dcaa3b27c5ce7885fea284b85189
Philipp Tempel <phtempel@gmail.com>
2013-11-21 10:04:19 +0100

    Add Orm\Observer_Auditor to fuel/app

---
+++ fuel/app/classes/orm/observer/auditor.php
@@ -0,0 +1,17 @@
+<?php namespace Orm;
+
+/**
+ * Part of the Gasoline framework
+ *
+ * @package     Gasoline
+ * @version     1.0-dev
+ * @author      Gasoline Development Teams
+ * @license     MIT License
+ * @copyright   2013 Gasoline Development Team
+ * @link        http://hubspace.github.io/gasoline
+ */
+
+class Observer_Auditor extends \Gasoline\Orm\Observer_Auditor {}
+
+/* End of file auditor.php */
+/* Location: ./fuel/app/classes/orm/observer/auditor.php */

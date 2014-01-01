bba601c0e6382d4baa14434ff45d50ae2e3f8e83
Philipp Tempel <phtempel@gmail.com>
2014-01-01 17:04:15 +0100

    Add \Controller\Frontend extends \Gasoline\Controller\Frontend

---
+++ fuel/app/classes/controller/frontend.php
@@ -0,0 +1,10 @@
+<?php namespace Controller;
+
+abstract class Frontend extends \Gasoline\Controller\Frontend {
+    
+    
+    
+}
+
+/* End of file frontend.php */
+/* Location: ./fuel/app/classes/controller/frontend.php */

a35d0057b9c463b1af93ec7e58a89210a60d946d
Philipp Tempel <phtempel@gmail.com>
2014-01-09 18:49:15 +0100

    Fix some bugs in \Gasoline\Controller\Frontend

--- fuel/gasoline/classes/controller/frontend.php
+++ fuel/gasoline/classes/controller/frontend.php
@@ -1,12 +1,12 @@
 <?php namespace Gasoline\Controller;
 
-abstract class Frontend extends Public {
+abstract class Frontend extends Base {
     
     public function before()
     {
         parent::before();
         
-        \Breadcrumb\Container::instance()->set('/', new \Breadcrumb\Crumb(array('href' => 'admin', 'text' => 'Home', 'attributes' => array())));
+        \Breadcrumb\Container::instance()->set('/', new \Breadcrumb\Crumb(array('href' => ( array_key_exists('_root_', \Router::$routes) ? \Router::$routes['_root_']->translation : \Config::get('routes._root_', '/') ), 'text' => 'Home', 'attributes' => array())));
     }
     
 }

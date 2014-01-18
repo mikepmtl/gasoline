b6d75907f2d2d90902bca89ccf7d5d0e6f8f8002
Philipp Tempel <phtempel@gmail.com>
2014-01-18 10:46:25 +0100

    Add \Fuel extends \Gasoline\Fuel

---
+++ fuel/app/classes/fuel.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Part of the Gasoline framework
+ *
+ * @package     Gasoline
+ * @version     1.0-dev
+ * @author      Gasoline Development Teams
+ * @license     MIT License
+ * @copyright   2013 Gasoline Development Team
+ * @link        http://hubspace.github.io/gasoline
+ */
+
+class Fuel extends \Gasoline\Fuel {
+    
+}
